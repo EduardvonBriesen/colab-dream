6b4951e24ea8db26fbf43b7dc1a271ddf7b91b1c
hanna.k <hanna.k@MacBook-Pro-2.local>
2023-03-10 12:18:58 +0100

    run python that can handle images

--- dream-connector/main.py
+++ dream-connector/main.py
@@ -1,2 +1,22 @@
+import sys
+from PIL import Image,ImageDraw
+
 if __name__ == "__main__":
-    print("hello")
+
+    args = sys.argv
+    # ronny, take this 
+    prompt = args[1]
+    
+   
+   
+  
+    
+    image = Image.new("RGB", (100, 100), (255, 0, 0))
+
+    # Draw a blue rectangle in the center of the image
+    draw = ImageDraw.Draw(image)
+    draw.rectangle((40, 40, 60, 60), fill=(0, 0, 255))
+
+    # Save the image as a PNG file
+    image.save("output.png", "PNG")
+  
