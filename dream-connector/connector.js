81174d4c95e3f15cfca6a7d38341e3e024eabc86
rkohlhaus <40360006+rkohlhaus@users.noreply.github.com>
2023-03-14 07:42:29 +0100

    Integrate python script; add error listener

--- dream-connector/connector.js
+++ dream-connector/connector.js
@@ -2,15 +2,24 @@ const { spawn } = require("child_process");
 
 exports.readJson = (req, res) => {
   //do something
+  console.log("Request received");
   console.log(req.body);
   var largeDataSet = [];
+
   // spawn new child process to call the python script
-  const python = spawn("python3", ["main.py", req.body]);
+  const python = spawn("python", ["../dream-braider/generator.py", "local", Object.values(req.body)]);
+
   // collect data from script
-  python.stdout.on("data", function (data) {
+  python.stdout.on('data', (data) => {
     console.log("Pipe data from python script ...");
     largeDataSet.push(data);
   });
+  
+  // Listen for data on the standard error stream
+  python.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+  
   // in close event we are sure that stream is from child process is closed
   python.on("close", (code) => {
     console.log(`child process close all stdio with code ${code}`);
