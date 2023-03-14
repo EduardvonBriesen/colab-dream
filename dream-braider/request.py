16b855f8162b388ac15650a72fd8c97948384854
rkohlhaus <40360006+rkohlhaus@users.noreply.github.com>
2023-03-14 07:25:12 +0100

    Optimize path handling

--- dream-braider/request.py
+++ dream-braider/request.py
@@ -1,16 +1,14 @@
 import helper.settings_parser as Parser
 
-def video_request(prompt):
-    string_array = Parser.load_file_to_array(path='struct/video.json')
+def video_request(prompt, pwd=''):
+    if pwd != '':
+        print('pwd: ', pwd)
+        string_array = Parser.load_file_to_array(path=pwd + '/struct/video.json')
+    else: string_array = Parser.load_file_to_array(path='struct/video.json')    
 
     string_array[73] = '"{' + r"\"0\": \"" + f'{prompt}' + r'\"' + '}",'
 
     request_string = "\n".join(string_array)
 
-    with open('struct/request.json', 'w') as file:
+    with open(pwd + '/struct/request.json', 'w') as file:
         file.write(request_string)
-
-    # return request_string
-
-# output = video_request('House on the street')
-# print(output)
