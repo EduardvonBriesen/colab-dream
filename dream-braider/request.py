9d45b0a6b79a1fdb898e6d80d33e1d581b929e7d
rkohlhaus <40360006+rkohlhaus@users.noreply.github.com>
2023-03-14 09:09:57 +0100

    Add data fields

--- dream-braider/request.py
+++ dream-braider/request.py
@@ -6,7 +6,41 @@ def video_request(prompt, pwd=''):
         string_array = Parser.load_file_to_array(path=pwd + '/struct/video.json')
     else: string_array = Parser.load_file_to_array(path='struct/video.json')    
 
+    # 2D or 3D
+    string_array[7] = '"3D",'
+
+    # images per prompt
+    string_array[8] = '20,'
+
+    # tranZ
+    string_array[14] = '"0: (3*(sin(3.141*t/50)**3)+.75)",'
+
+    # rotX
+    string_array[15] = '"0: (0.25*sin(2*3.141*t/250))",'
+
+    # rotY
+    string_array[16] = '"0: (0.25*sin(2*3.141*t/250))",'
+
+    # rotZ
+    string_array[17] = '"0: (0.5*sin(2*3.141*t/250))",'
+
+    # strength
+    string_array[24] = '"0: (-0.13*(cos(3.141*t/13)**100)+0.63)",'
+
+
     string_array[73] = '"{' + r"\"0\": \"" + f'{prompt}' + r'\"' + '}",'
+
+    # steps
+    string_array[85] = '20,'
+
+    # output folder
+    string_array[93] = '"dream",'
+
+    # skip video stiching: true or false
+    string_array[114] = 'true,'
+
+    # FPS
+    string_array[115] = '8,'
 
     request_string = "\n".join(string_array)
 
