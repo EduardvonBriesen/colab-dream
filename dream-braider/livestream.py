cd8eccb1b0abb09df753e597d3b76ba9911fa5ee
rkohlhaus <40360006+rkohlhaus@users.noreply.github.com>
2023-11-03 19:49:18 +0000

    added continous video livestream

--- dream-braider/livestream.py
+++ dream-braider/livestream.py
@@ -1,39 +1,43 @@
 import os
-from flask import Flask, render_template, Response, jsonify
+from flask import Flask, render_template, Response, jsonify, request
 
 app = Flask(__name__)
 
 # Define the path to the directory where videos are located
-video_directory = '/workspace/stable-diffusion-webui/outputs/img2img-images/dream/'
+video_directory = '/workspace/stable-diffusion-webui/outputs/img2img-images/dream_4/'
 
 def get_video_files(directory):
     video_files = [os.path.join(directory, filename) for filename in os.listdir(directory) if filename.endswith('.mp4')]
     sorted_videos = sorted(video_files, key=lambda x: os.path.getctime(x))
     return sorted_videos
 
+
+video_index = 0
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/get_video_list')
-def get_video_list():
-    video_files = get_video_files(video_directory)
-    return jsonify(video_files)
+def generate_video(index):
+    video_path = os.path.join(get_video_files(video_directory)[index])
+    
+    with open(video_path, 'rb') as video:
+        while True:
+            chunk = video.read(1024)
+            if not chunk:
+                break
+            yield chunk
 
-def generate():
-    video_files = get_video_files(video_directory)
-    for video_file in video_files:
-        with open(video_file, 'rb') as video:
-            print(video_file)
-            while True:
-                chunk = video.read(512)
-                if not chunk:
-                    break
-                yield chunk
+@app.route('/video_feed')
+def video_feed():
+    index = int(request.args.get('index', 0))
+    return Response(generate_video(index), mimetype='video/mp4')
 
-@app.route('/video')
-def video():
-    return Response(generate(), content_type='video/mp4')
+@app.route('/next_video')
+def next_video():
+    global video_index
+    video_index += 1 # TODO: Break condition
+    return 'Switching to the next video'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
