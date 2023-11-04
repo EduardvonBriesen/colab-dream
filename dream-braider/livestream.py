12e9e2f8954956957bff514a2293772ccee67d7b
Eduard von Briesen <e.v.briesen@gmail.com>
2023-11-04 11:17:22 +0100

    Update video directory and adjust playback speed

--- dream-braider/livestream.py
+++ dream-braider/livestream.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, Response, jsonify, request
 app = Flask(__name__)
 
 # Define the path to the directory where videos are located
-video_directory = '/workspace/stable-diffusion-webui/outputs/img2img-images/dream/'
+video_directory = './video'
 
 def get_video_files(directory):
     video_files = [os.path.join(directory, filename) for filename in os.listdir(directory) if filename.endswith('.mp4')]
