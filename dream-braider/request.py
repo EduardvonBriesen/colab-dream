87ec26f5d32b0031da67abbf17e8781192211481
rkohlhaus <40360006+rkohlhaus@users.noreply.github.com>
2023-11-05 12:27:48 +0000

    Adapt model arguments to new style

--- dream-braider/request.py
+++ dream-braider/request.py
@@ -8,18 +8,19 @@ def video_request(prompt, pwd='', settings=None):
 
     # add style specifying keywords
     prompt = Parser.modify_prompt_randomly(input=prompt)
+    prompt = "masterpiece, visually specific, high resolution, 4k, 8k, high res, clarity, well balanced contrast, strong contrast" + prompt
 
     # get last image of previous prompt
     # TODO integrate settings from config file
     if settings is not None:
         first_run, init_image = Parser.get_init_image(folder=settings['APP']['path_dreams'])
-    else: first_run, init_image = Parser.get_init_image(folder="/home/ubuntu/stable-diffusion-webui/outputs/img2img-images/dream")
+    else: first_run, init_image = Parser.get_init_image(folder="/workstation/stable-diffusion-webui/outputs/img2img-images/dream")
 
     # 2D or 3D
-    string_array[36] = '"animation_mode": "3D",'
+    string_array[36] = '"animation_mode": "2D",'
 
     # images per prompt
-    string_array[37] = '"max_frames": 30,'
+    string_array[37] = '"max_frames": 120,'
 
     # tranZ
     string_array[43] = '"translation_z": "0: (3*(sin(3.141*t/50)**3)+.75)",'
@@ -44,7 +45,7 @@ def video_request(prompt, pwd='', settings=None):
     # string_array[33] = '"prompts": '+ '"{' + '": "' + f'{prompt}' + '"' + '}",'
 
     # steps
-    string_array[11] = '"steps": 20,'
+    string_array[11] = '"steps": 15,'
 
     # output folder
     string_array[12] = '"batch_name": "dream",'
@@ -52,23 +53,23 @@ def video_request(prompt, pwd='', settings=None):
     # use init image: true or false
     # first run does not have an init image
     if first_run: string_array[15] = '"use_init": false,'
-    else: string_array[15] = '"use_init": true,'
+    else: string_array[15] = '"use_init": false,'
 
     # init image strength
     # TODO check if other values are more suitable
-    string_array[16] = '"strength": 0.6,'
+    string_array[16] = '"strength": 0.5,'
 
     # init image file
     if first_run: string_array[18] = '"init_image": null,'
     else: string_array[18] = f'"init_image": "{init_image}",'
 
     # skip video stiching: true or false
-    string_array[234] = '"skip_video_creation": true,'
+    string_array[234] = '"skip_video_creation": false,'
 
     # FPS
-    string_array[235] = '"fps": 8,'
+    string_array[235] = '"fps": 5,'
 
-    string_array[252] = '"sd_model_name": "v1-5-pruned.ckpt",'
+    string_array[252] = '"sd_model_name": "mdjrny-v4.ckpt",'
 
     request_string = "\n".join(string_array)
 
